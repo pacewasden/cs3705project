0221f8869f45c60eb7d481a00b51179aafac65c7
PACE WASDEN <pacewasden@gmail.com>
2021-02-15 11:34:43 -0700

    Add files via upload
    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+addresses = {
+    "mac": ['82:13:0b:06:45:41 ','82:13:0b:06:45:44 '],
+    "ip": ['192.0.0.1', '192.0.0.2' ]
+}
+
+myvar= pd.DataFrame(addresses, index=['row1','row2'])
+print(myvar['row2'])
